0054155156dc50a56245b179a66611e0277560b6
RoseKeenan <rosemkeenan@gmail.com>
2019-03-26 11:50:44 -0400

    Added footers titles to efforts

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -4,14 +4,14 @@ if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['message']) |
   http_response_code(500);
   exit();
 }
-
+method = "post"
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email = strip_tags(htmlspecialchars($_POST['email']));
 #$phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = "starscc.temple@gmail"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = "tuh05910@temple.edu"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
 $subject = "Website Contact Form:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
 $header = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
